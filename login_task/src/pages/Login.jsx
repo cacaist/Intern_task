1ec7efe3030d336d0181a7fa3171d2442a664236
Çağatay Kınacı <su-cagatay@hotmail.com>
2025-12-02 00:54:43 +0300

    v1.5.122 (Basic Login)

--- login_task/src/pages/Login.jsx
+++ login_task/src/pages/Login.jsx
@@ -1,4 +1,67 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
 function Login() {
-  return <h1>Login Page</h1>;
+  // States
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Validation
+    if (email === "" || password === "") {
+      setError("Maybe you should type something first?");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      setError("Are you sure that's an email?");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Dont be lazy, go for least 6 characters password!");
+      return;
+    }
+
+    // If everything is ok
+    setError("");
+    console.log("Login Successful:", { email, password });  
+    // Navigate to other page
+    navigate('/welcome', { state: { userEmail: email } });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      
+      <form onSubmit={handleLogin} noValidate className="login-form">
+        <input 
+          type="email" 
+          placeholder="Your Email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p className="error-text">{error}</p>}
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
+
 export default Login;
--- login_task/src/pages/Welcome.jsx
+++ login_task/src/pages/Welcome.jsx
@@ -1,4 +1,16 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 function Welcome() {
-  return <h1>Welcome</h1>;
+  const location = useLocation();
+  const email = location.state?.userEmail || "Casper";
+
+  return (
+    <div >
+      <h1>Welcome! {email}</h1>
+      
+    </div>
+  );
 }
+
 export default Welcome;

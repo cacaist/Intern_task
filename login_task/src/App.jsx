3d3069fe466c92c5e3918610acb73f2a0e6d1851
Çağatay Kınacı <su-cagatay@hotmail.com>
2025-12-01 19:18:43 +0300

    first commit

---
+++ login_task/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Welcome from './pages/Welcome';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/welcome" element={<Welcome />} />
+    </Routes>
+  );
+}
+export default App;
